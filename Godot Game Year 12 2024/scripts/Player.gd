966de77b42692b8878e6713c8b6060e92da8a1b0
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2024-06-26 11:15:50 +1200

    Most of player 1 coding
    Crouch, sprint, and lerp done

--- Godot Game Year 12 2024/scripts/Player.gd
+++ Godot Game Year 12 2024/scripts/Player.gd
@@ -1,28 +1,48 @@
 extends CharacterBody3D
 
 
+@onready var head = $Head
+
 var current_speed = 5.0
 
-const walking_speed = 5.0
+var lerp_speed  = 10.0
+var direction = Vector3.ZERO
+
+const walking_speed = 10.0
 const sprinting_speed = 25.0
-const crouching_speed = 3.0
+const crouching_speed = 5.0
 
 const jump_velocity = 4.5
 
 const mouse_sens = 0.4
 
+var crouching_depth = -0.5
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	Input.set_mouse_mode(INPUT.MOUSE_MODE_CAPTURE):
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
+		head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
 
 func _physics_process(delta):
 	
-	if Input.is_action_just_pressed("sprint"):
-		current_speed = sprinting_speed
+	
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y,1.8 + crouching_depth,delta*lerp_speed)
 	else:
-		current_speed = walking_speed
+		head.position.y = lerp(head.position.y,1.8,delta*lerp_speed)
+		if Input.is_action_just_pressed("sprint"):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
 	
 	
 	# Add the gravity.
@@ -36,7 +56,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
